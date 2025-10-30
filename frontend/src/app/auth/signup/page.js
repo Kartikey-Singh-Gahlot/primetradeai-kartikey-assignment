c7a390b568f4fe1a7e27a51faa138796ab3512c6
Kartikey <decten.kartz@gmail.com>
2025-10-30 16:42:14 +0530

    styled frontend credential forms

--- frontend/src/app/auth/signup/page.js
+++ frontend/src/app/auth/signup/page.js
@@ -1,7 +1,51 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 export default function signup(){
+  
+    const router = useRouter();
+
+    useEffect(()=>{
+       async function get(){
+        const unp = await fetch(`${process.env.NEXT_PUBLIC_BACKENDURL}/auth/checkAuth`, {method:"GET", credentials:"include"});
+        const pr = await unp.json();
+        if(pr.status){
+         router.push("/"); 
+        }
+        else{
+            return;
+        }
+       }
+       get();
+    },[])
+
+
+    function trgrChange(event){
+        setFormData((prev)=>{
+           return {...prev, [event.target.name]:event.target.value}
+        });
+    }
+
+    const [formData, setFormData] = useState({name:"", email:"", password:"", adminKey:""});
+
     return(
-        <main>
-             <h1>Signup page</h1>
+        <main className="h-screen overflow-hidden">
+             <header className="formNavBar"> </header>
+             <div className="formWrapper">
+                 <form className="formBox">
+                       <input className="credentialInput" name="name" onChange={(e)=>{trgrChange(e)}} placeholder="Enter your name" value={formData.name}/>
+                       <input className="credentialInput" name="email" onChange={(e)=>{trgrChange(e)}} placeholder="Enter your email" value={formData.email}/>
+                       <input className="credentialInput" name="password" onChange={(e)=>{trgrChange(e)}} placeholder="Enter your password" value={formData.password}/>
+                        <div className="buttonStrip ">
+                              <h1 className="showPassButton">Show Password</h1>
+                             <button className="submitButton">SignUp</button>
+                       </div>
+                       <h1 className="suggestionText">Already have an account ? <Link href="/auth/signin">Login</Link></h1>
+                 </form>
+             </div>
+             
         </main>
     )
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,29 @@
-import Image from "next/image";
+"use client"
+import {useState, useEffect} from 'react';
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+ const router = useRouter();
+
+  useEffect(()=>{
+    async function checkAuth() {
+      const unp = await fetch(`${process.env.NEXT_PUBLIC_BACKENDURL}/auth/checkAuth`, {method:"GET", credentials:"include"});
+      const pr = await unp.json();
+      console.log(pr.status);
+      if(!pr.status){
+        router.push("/auth/signup");
+      }
+    }
+    checkAuth();
+  },[])
+
+
+
   return (
    <main>
-       <h1>Home</h1>
+       <h1>Notes</h1>
+
    </main>
   );
 }
